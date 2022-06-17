0d28c0db081cbd0ad7ed8b160e6dc639ca18b6d1
gomez-fernando <ferdgomez@hotmail.es>
2022-06-17 20:08:23 +0200

    Add lazy loading and routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from '../features/counter/Counter';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from 'react-router-dom';
+import { Layout } from '../components/core/layout';
+import { aMenuItems } from '../interfaces/menu-items';
+import * as React from 'react';
+// import { HomePage } from '../pages/homePage';
+// import { GotContextProvider } from './context/got-provider';
+// import { TodoContextProvider } from './context/todo-provider';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    const HomePage = React.lazy(() => import('../pages/homePage'));
+    const CoffePage = React.lazy(() => import('../pages/coffeePage'));
+
+    const options: aMenuItems = [
+        { path: '', label: 'Home - Ofertas', page: <HomePage /> },
+        { path: 'coffee', label: 'Cafés', page: <CoffePage /> },
+        { path: '*', label: '', page: <Navigate replace to="" /> },
+    ];
+
+    console.log(options);
+    return (
+        <Router>
+            <Layout options={options}>
+                <React.Suspense>
+                    {/* <GotContextProvider>
+                        <TodoContextProvider> */}
+                            <Routes>
+                                {options.map((item) => (
+                                    <Route
+                                        key={item.label}
+                                        path={item.path}
+                                        element={item.page}
+                                    ></Route>
+                                ))}
+                            </Routes>
+                        {/* </TodoContextProvider>
+                    </GotContextProvider> */}
+                </React.Suspense>
+            </Layout>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/core/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer>
+            <address>ISDI Coders</address>
+        </footer>
+    );
+}
