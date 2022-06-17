8e3ab549d3451eb889808a15759fd51b880a5b9d
gomez-fernando <ferdgomez@hotmail.es>
2022-06-17 18:58:03 +0200

    Add test for reducer

---
+++ src/models/product.ts
@@ -0,0 +1,29 @@
+export enum sizes{
+    'S', 'M', 'L', 'XL'
+}
+
+export enum categories{
+    'coffe', 'tea'
+}
+
+export class Product{
+
+    static generateId(): number {
+        return Math.ceil(Math.random() * 100_000);
+    }
+
+    id: number;
+    promo: boolean;
+
+    constructor(
+        public price: number,
+        public size: sizes,
+        public origin: string,
+        public image: string,
+        public description: string,
+        public category: categories
+    ){
+        this.id = Product.generateId();
+        this.promo = false;
+    }
+}
